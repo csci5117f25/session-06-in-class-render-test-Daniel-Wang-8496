f7466bc4a2046b279e9c6700c8aa8855ed1606ac
Daniel Wang <danwang763@gmail.com>
2025-09-25 09:04:24 -0500

    Added database connection, guestlist form

---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE guest_list (
+    Name varchar(255),
+    Message varchar(255)
+);
--- server.py
+++ server.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import os
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('index.html', name=name)
+
+@app.route('/guestlist', methods=['POST'])
+def gfg():
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+    if request.method == "POST":
+        name = request.form.get("name")
+        message = request.form.get("message")
+        with conn.cursor() as cur:
+            cur.execute("CREATE TABLE guest_list (Name varchar(255), Message varchar(255))")
+            cur.execute("INSERT INTO guest_list (Name, Message) VALUES (%s, %s)", (name, message))
+    conn.close()
+    return render_template('index.html', name=name, message=message)
